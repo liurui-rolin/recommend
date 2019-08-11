824f97c9e39f5866dff38bc28998d388a0a63d8b
ruiliu <946578327@qq.com>
2019-08-11 23:45:14 +0800

    基于热度的推荐

--- src/main/scala/youling/studio/recommend/hotbase/HotBase.scala
+++ src/main/scala/youling/studio/recommend/hotbase/HotBase.scala
@@ -11,12 +11,20 @@ object HotBase {
     println("start...")
 
     val logFile = "data/SogouQ.sample"
-    val spark = SparkSession.builder.master("local[2]").appName("Simple Application").getOrCreate()
+    // 创建spark
+    val spark = SparkSession.builder.master("local[2]").appName("Hot base app").getOrCreate()
+    //读取数据
     val logData = spark.read.textFile(logFile)
     import spark.implicits._
-    val etlData = logData.map(_.toString.replace("[","").replace("]",""))
+    //简单清洗
+    val etlData = logData.map(_.toString.replace("[","").replace("]","")).cache()
 
+    //显示示例数据
     etlData.limit(10).collect().foreach(println(_))
+
+    //执行热词计算
+    val hotWords = etlData.map(line => (line.split("\t")(2),1)).rdd.reduceByKey((a,b) => a+b).map(res => (res._2,res._1)).sortByKey(false,1)
+    hotWords.take(100).foreach(println(_))
 
 
     spark.stop()
